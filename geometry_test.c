1977fac662a337263971ca36613ccb8e0574abf8
user <jdsanchez2011@gmail.com>
2015-12-02 16:16:43 -0700

    triangle area function and test

--- geometry_test.c
+++ geometry_test.c
@@ -145,6 +145,26 @@ START_TEST(test_2d_midpoint)
 }
 END_TEST
 
+/* coord_2d triangle area test */
+START_TEST(test_coord_2d_area_triangle) {
+    coord_2d_t a;
+    coord_2d_t b;
+    coord_2d_t c;
+    double area;
+
+    a.x = 10.;
+    a.y = 9.;
+    b.x = 10.;
+    b.y = 20.;
+    c.x = 40.;
+    c.y = 15.;
+
+    area = coord_2d_area_triangle(&a, &b, &c);
+
+    ck_assert(165. == area);
+}
+END_TEST
+
 /* coord_2d Test Suite */
 Suite* coord_2d_suite(void)
 {
@@ -162,10 +182,13 @@ Suite* coord_2d_suite(void)
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
 
+    TCase* tc_coord_2d_area_triangle = tcase_create("coord_2d_area_triangle");
+    tcase_add_test(tc_coord_2d_area_triangle, test_coord_2d_area_triangle);
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
     suite_add_tcase(s, tc_2d_dist);
     suite_add_tcase(s, tc_2d_midpoint);
+    suite_add_tcase(s, tc_coord_2d_area_triangle);
 
     /* Return Suite */
     return s;
